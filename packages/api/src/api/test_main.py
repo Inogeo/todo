3d06bb619c9e8568a88720af19229ac7e30c5327
p.hardy <pierre.hardy@egis-group.com>
2025-04-24 23:46:12 +0200

    fix: adjusted test to rely on a in memory database

--- packages/api/src/api/test_main.py
+++ packages/api/src/api/test_main.py
@@ -1,10 +1,33 @@
+import pytest  
 from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
-from api.main import app
+from .main import app
+from .database import get_session
 
-client = TestClient(app)
+# In memory db used for tests
+@pytest.fixture(name="session")  
+def session_fixture():  
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
 
-def test_heathcheck():
+@pytest.fixture(name="client")  
+def client_fixture(session: Session):  
+    def get_session_override():  
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override  
+
+    client = TestClient(app)  
+    yield client  
+    app.dependency_overrides.clear()
+
+def test_heathcheck(client: TestClient):
     """Test healthcheck endpoint."""
     response = client.get("/")
     assert response.status_code == 200
